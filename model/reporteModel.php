1dfb7c7febc7dde3a006fa4324d36a62bf75a28d
mauroayala <mauro.julian.ayala@gmail.com>
2021-11-22 19:41:41 -0300

    Update reporteModel.php

--- model/reporteModel.php
+++ model/reporteModel.php
@@ -15,12 +15,12 @@ class reporteModel
        
         $query="SELECT SUM(valor) AS Facturacion_Mensual FROM vuelo WHERE fecha BETWEEN '$fecha1' AND '$fecha2'";
          return $this->dataBase->query($query);
-           }
+	}
 
-           public function reporteCabinaMasVendida(){
+    public function reporteCabinaMasVendida(){
          $query="SELECT CAB.nombre AS Cabina_mas_Vendida, COUNT(*) as CANTIDAD  FROM reserva RES JOIN cabina CAB ON RES.id_cabina=CAB.id_cabina
         WHERE estado ='abonado' GROUP BY  RES.id_cabina ORDER BY RES.id_cabina DESC;";
         return $this->dataBase->query($query);
-           }
+	}
 
 }
